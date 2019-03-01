e5522122690e2c974a8f4cd1599b6e9ec6d2b42f
nandy <nandy_wong@126.com>
2019-03-01 17:10:59 +0800

    去掉console定义

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ env.JQLite = require('./libs/JQLite.browser');
 if(!env.$) env.$ = env.JQLite;
 
 module.exports = env.JQLite;
-
-if(console.debug){
-    console.log = console.warn = console.error = console.debug;
-}
