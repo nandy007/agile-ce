629d41904beddbea7520c1d8cdf67e74cdfcde97
nandy <nandy_wong@126.com>
2019-03-01 17:01:32 +0800

    合并类似指令的算法。增加v-xclass和v-xstyle指令对应微信小程序指令

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ env.JQLite = require('./libs/JQLite.browser');
 if(!env.$) env.$ = env.JQLite;
 
 module.exports = env.JQLite;
+
+if(console.debug){
+    console.log = console.warn = console.error = console.debug;
+}
