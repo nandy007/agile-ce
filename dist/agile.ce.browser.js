1ffb958b2fb0bd11d370da8da07295f2400d7f0c
404391045@qq.com <404391045@qq.com>
2020-04-07 18:01:07 +0800

    优化数据监听

--- dist/agile.ce.browser.js
+++ dist/agile.ce.browser.js
@@ -1,6 +1,6 @@
 /*
  *	Agile CE 移动前端MVVM框架
- *	Version	:	0.5.25.1585017089165 beta
+ *	Version	:	0.6.0.1586253601354 beta
  *	Author	:	nandy007
  *	License MIT @ https://github.com/nandy007/agile-ce
  *//******/ (function(modules) { // webpackBootstrap
--- dist/agile.ce.native.js
+++ dist/agile.ce.native.js
@@ -1,6 +1,6 @@
 /*
  *	Agile CE 移动前端MVVM框架
- *	Version	:	0.5.25.1585017094417 beta
+ *	Version	:	0.6.0.1586253606850 beta
  *	Author	:	nandy007
  *	License MIT @ https://github.com/nandy007/agile-ce
  */var __ACE__ = {};
